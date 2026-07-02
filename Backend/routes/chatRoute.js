ccbfa0125c4fbe53dd126809b09706cf456da3ff
Muhammad Uvais <uvaisakram95@gmail.com>
2026-07-02 18:39:59 +0530

    refactor: remove temporary test route

--- Backend/routes/chatRoute.js
+++ Backend/routes/chatRoute.js
@@ -19,29 +19,4 @@ router.post("/chat", gettingResponse);
 
 
 
-
-
-
-router.get("/test", async(req, res) => {
-    try{
-         const thread = new Thread({
-        threadId: "Uvais123456",
-        title: "What is Python",
-        messages: [{
-            role: "user",
-            content: "Library of python"
-        }]
-    })
-        await thread.save();
-        return res.status(201).json({message: "new thread created successfully!", thread})
-    }catch(err) {
-      console.log("error is ::", err);
-    }
-   
-})
-
-
-
-
-
 export default router;
